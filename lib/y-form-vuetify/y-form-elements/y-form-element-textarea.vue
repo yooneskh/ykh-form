960d83bbeb99c50a8e03559470635d49d82d306c
Yoones Khoshghadam <yooneskh@gmail.com>
2021-11-30 18:25:37 +0330

    fix (locale): locales key change

--- lib/y-form-vuetify/y-form-elements/y-form-element-textarea.vue
+++ lib/y-form-vuetify/y-form-elements/y-form-element-textarea.vue
@@ -21,16 +21,16 @@
     :hint="typeof field.hint === 'function' ? field.hint(value) : field.hint"
     persistent-hint
     :hide-details="!field.message && !field.hint">
-    <template v-if="field.languages" #append>
+    <template v-if="field.locales" #append>
 
-      <v-menu v-if="field.languages" absolute>
+      <v-menu v-if="field.locales" absolute>
         <template #activator="{ on, attrs }">
           <v-btn rounded small text v-on="on" v-bind="attrs" class="px-2" min-width="0">
             {{ currentLanguage }}
           </v-btn>
         </template>
         <v-list dense>
-          <v-list-item v-for="(value, key) of field.languages" :key="key" @click="changeLanguageTo(key)">
+          <v-list-item v-for="(value, key) of field.locales" :key="key" @click="changeLanguageTo(key)">
             <v-list-item-content>
               <v-list-item-title>{{ key }}</v-list-item-title>
             </v-list-item-content>
@@ -62,7 +62,7 @@ export default {
   }),
   computed: {
     currentValue() {
-      if (!this.field.languages) return this.value;
+      if (!this.field.locales) return this.value;
 
       if (!this.value) return '';
       return this.value[this.currentLanguage];
@@ -70,8 +70,8 @@ export default {
     }
   },
   created() {
-    if (this.field.languages) {
-      this.currentLanguage = Object.keys(this.field.languages)[0];
+    if (this.field.locales) {
+      this.currentLanguage = Object.keys(this.field.locales)[0];
       if (!this.value) this.$emit('input', {});
     }
   },
@@ -87,7 +87,7 @@ export default {
     async handleInput(text) {
       if (this.currentLanguageChanged) return;
 
-      if (this.field.languages) {
+      if (this.field.locales) {
         this.$set(this.value, this.currentLanguage, text);
         this.$emit('input', this.value);
         await this.$nextTick();
